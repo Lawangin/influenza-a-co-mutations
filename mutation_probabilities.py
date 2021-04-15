7beea4464756bcfc6a2c48b1cee5fab62714b94d
Lawangin <lawangin.khan@gmail.com>
2021-04-14 22:15:15 -0400

    add mutation probabilities and co-mutations

--- mutation_probabilities.py
+++ mutation_probabilities.py
@@ -1,27 +1,58 @@
+import numpy as np
 data = []
-with open('newyork_records.txt', 'r') as reader:
+with open('newyork_records_2003.txt', 'r') as reader:
     for line in reader:
         if line[0] == 'M':
             data.append(line.strip())
 
 
 def get_freq(d):
+    matrix = np.array(d)
     freq_per_seq = []
-    freq = {}
-    for seq in range(len(d)):
-    # for seq in range(3):
-        for resi in range(len(d[0])):
-        # for resi in range(3):
-            if d[seq][resi] in freq:
-                freq[d[seq][resi]] += 1
+
+    freq_matrix = {}
+    for seq in range(len(d[0])):
+        res = [sub[seq] for sub in d]
+        for i in res:
+            if i in freq_matrix:
+                freq_matrix[i] += 1
             else:
-                freq[d[seq][resi]] = 1
-        # print(freq)
-        # freq_per_seq.append(freq)
-        # freq ={}
-    return freq
+                freq_matrix[i] = 1
+        freq_per_seq.append(freq_matrix)
+        freq_matrix = {}
+    return freq_per_seq
 
 
+def get_prob(freq):
+    mutations = []
+    for i in range(len(freq)):
+        if len(freq[i]) > 1:
+            freq_values = freq[i].values()
+            highest_n = max(freq[i].values())
+            den = 90
+            num = 0
+            for n in freq_values:
+                if n != highest_n:
+                    num += n
+            probability = num/den
 
-# get_freq(data)
-print(get_freq(data))
+            mutations.append({'location': i, 'mutation': freq[i], 'probability': probability})
+    return mutations
+
+
+freq = get_freq(data)
+prob = get_prob(freq)
+# get_prob(freq)
+
+co_mut = []
+for j in range(len(prob)):
+    control = prob[j]['probability']
+    for i in range(len(prob)):
+        if j != i:
+            if prob[i]['probability'] == control:
+                co_mut.append([prob[j], prob[i]])
+
+# print(prob)
+# print(co_mut)
+print(len(prob))
+print(len(co_mut))
